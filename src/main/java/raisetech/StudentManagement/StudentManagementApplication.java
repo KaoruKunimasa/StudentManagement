489cd71a86d194ad9a9da66d2f06f8ce27269777
Kaoru Kunimasa <>
2025-06-01 23:57:37 +0900

    MyBatisを使ってCRUDのRead処理を実装しました。

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,15 +1,10 @@
 package raisetech.StudentManagement;
 
 import java.util.List;
-import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -23,37 +18,15 @@ public class StudentManagementApplication {
     SpringApplication.run(StudentManagementApplication.class, args);
   }
 
-  @GetMapping("/student")
-  public String getStudent(@RequestParam String name) {
-    Student student = repository.searchByName(name);
-    return student.getName() + " " + student.getAge() + "歳";
+  @GetMapping("/studentList")
+  public List<Student> getStudentList() {
+    return repository.search();
   }
 
-  @PostMapping("/student")
-  public void registerStudent(String name, int age) {
-    repository.registerStudent(name, age);
+  @GetMapping("/studentCourseList")
+  public List<StudentCourse> getStudentCourseList() {
+    return repository.courseSearch();
   }
 
-  @PatchMapping("/student")
-  public void updateStudent(String name, int age) {
-    repository.updateStudent(name, age);
-  }
-
-  @DeleteMapping("/student")
-  public void deleteStudent(String name) {
-    repository.deleteStudent(name);
-  }
-
-  @GetMapping("/students")
-  public String getAllStudents() {
-    List<Student> students = repository.findAll();
-
-    // Stream APIで文字列結合
-    String result = students.stream()
-        .map(student -> student.getName() + " " + student.getAge() + "歳")
-        .collect(Collectors.joining("\n")); // 改行区切りで結合
-
-    return result;
-  }
 
 }
