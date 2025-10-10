0ca4a6114b8ea27f9c14d09ff51dbe893c159387
Kaoru Kunimasa <>
2025-10-10 15:49:36 +0900

    エラー処理を実装しました。

---
+++ src/main/java/raisetech/StudentManagement/exceptionhandler/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package raisetech.StudentManagement.exceptionhandler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler(TestException.class)
+  public ResponseEntity<String> handleTestException(TestException ex) {
+    return ResponseEntity
+        .status(HttpStatus.BAD_REQUEST)
+        .body(ex.getMessage());
+  }
+}
